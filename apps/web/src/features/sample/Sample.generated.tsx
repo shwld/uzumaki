5a01b68b9454600426e22efc684b33aa2afbd672
shwld <shu.account@outlook.com>
2022-05-31 09:04:07 +0900

    codegen

--- apps/web/src/features/sample/Sample.generated.tsx
+++ apps/web/src/features/sample/Sample.generated.tsx
@@ -3,18 +3,18 @@ import * as Types from '../../graphql/generated/graphql';
 import gql from 'graphql-tag';
 import * as Urql from 'urql';
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
-export type SampleQueryVariables = Types.Exact<{ [key: string]: never }>;
+export type SampleQueryVariables = Types.Exact<{ [key: string]: never; }>;
 
-export type SampleQuery = { __typename?: 'Query'; sample?: string | null };
+
+export type SampleQuery = { __typename?: 'Query', sample?: string | null };
+
 
 export const SampleDocument = gql`
-  query sample {
-    sample
-  }
-`;
-
-export function useSampleQuery(
-  options?: Omit<Urql.UseQueryArgs<SampleQueryVariables>, 'query'>
-) {
-  return Urql.useQuery<SampleQuery>({ query: SampleDocument, ...options });
+    query sample {
+  sample
 }
+    `;
+
+export function useSampleQuery(options?: Omit<Urql.UseQueryArgs<SampleQueryVariables>, 'query'>) {
+  return Urql.useQuery<SampleQuery>({ query: SampleDocument, ...options });
+};
