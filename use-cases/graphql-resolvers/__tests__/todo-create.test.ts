41f22844986ce7f02a5afb2169e2cab2b5583be8
shwld <shu.account@outlook.com>
2022-06-10 08:54:53 +0900

    🚨test: add use case test

---
+++ use-cases/graphql-resolvers/__tests__/todo-create.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from 'vitest';
+
+test('query is defined', () => {
+  expect('hoge').toEqual('hoge');
+});
--- use-cases/graphql-resolvers/src/graphql/shared/rules/isAuthenticated.ts
+++ use-cases/graphql-resolvers/src/graphql/shared/rules/isAuthenticated.ts
@@ -1,12 +1,7 @@
 import { rule } from 'graphql-shield';
 import { RuleFunction } from '../types';
 
-const isAuthenticatedRule: RuleFunction = async (
-  _parent,
-  _args,
-  ctx,
-  _info
-) => {
+const isAuthenticatedFn: RuleFunction = async (_parent, _args, ctx, _info) => {
   return ctx.currentUser != null;
 };
-export const isAuthenticated = rule()(isAuthenticatedRule);
+export const isAuthenticated = rule()(isAuthenticatedFn);
