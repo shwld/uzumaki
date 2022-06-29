4d88c668f97ad752bce28d9b958cfa8026aa0fd6
shwld <shu.account@outlook.com>
2022-06-30 05:43:50 +0900

    👷chore: codegen

--- apps/web/src/components/todo/UpdateTodoTitleForm/UpdateTodoTitleForm.generated.tsx
+++ apps/web/src/components/todo/UpdateTodoTitleForm/UpdateTodoTitleForm.generated.tsx
@@ -3,49 +3,33 @@ import * as Types from '../../../graphql/generated/graphql';
 import gql from 'graphql-tag';
 import * as Urql from 'urql';
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
-export type UpdateTodoTitleFormFragment = {
-  __typename?: 'Todo';
-  id: string;
-  title: string;
-};
+export type UpdateTodoTitleFormFragment = { __typename?: 'Todo', id: string, title: string };
 
 export type TodoUpdateTitleFormMutationVariables = Types.Exact<{
   input: Types.UpdateTodoTitleInput;
 }>;
 
-export type TodoUpdateTitleFormMutation = {
-  __typename?: 'Mutation';
-  updateTodoTitle:
-    | { __typename?: 'InvalidArgumentsResult' }
-    | { __typename?: 'UnauthorizedResult' }
-    | {
-        __typename?: 'UpdateTodoTitleSuccessResult';
-        result: { __typename?: 'Todo'; id: string; title: string };
-      };
-};
+
+export type TodoUpdateTitleFormMutation = { __typename?: 'Mutation', updateTodoTitle: { __typename?: 'InvalidArgumentsResult' } | { __typename?: 'UnauthorizedResult' } | { __typename?: 'UpdateTodoTitleSuccessResult', result: { __typename?: 'Todo', id: string, title: string } } };
 
 export const UpdateTodoTitleFormFragmentDoc = gql`
-  fragment UpdateTodoTitleForm on Todo {
-    id
-    title
-  }
-`;
+    fragment UpdateTodoTitleForm on Todo {
+  id
+  title
+}
+    `;
 export const TodoUpdateTitleFormDocument = gql`
-  mutation todoUpdateTitleForm($input: UpdateTodoTitleInput!) {
-    updateTodoTitle(input: $input) {
-      ... on UpdateTodoTitleSuccessResult {
-        result {
-          ...UpdateTodoTitleForm
-        }
+    mutation todoUpdateTitleForm($input: UpdateTodoTitleInput!) {
+  updateTodoTitle(input: $input) {
+    ... on UpdateTodoTitleSuccessResult {
+      result {
+        ...UpdateTodoTitleForm
       }
     }
   }
-  ${UpdateTodoTitleFormFragmentDoc}
-`;
+}
+    ${UpdateTodoTitleFormFragmentDoc}`;
 
 export function useTodoUpdateTitleFormMutation() {
-  return Urql.useMutation<
-    TodoUpdateTitleFormMutation,
-    TodoUpdateTitleFormMutationVariables
-  >(TodoUpdateTitleFormDocument);
-}
+  return Urql.useMutation<TodoUpdateTitleFormMutation, TodoUpdateTitleFormMutationVariables>(TodoUpdateTitleFormDocument);
+};
