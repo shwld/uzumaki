cb4c4c7ed38667a2fcc301bd6a001ebc66e92e30
shwld <shu.account@outlook.com>
2022-07-09 07:30:32 +0900

    👷chore: yarn g:module story

--- use-cases/graphql-resolvers/src/index.ts
+++ use-cases/graphql-resolvers/src/index.ts
@@ -5,6 +5,7 @@ import { GraphqlServerContext } from './context';
 import { Resolvers } from './generated/resolversTypes';
 import { typeDefs } from './generated/typeDefs';
 import { accountModule } from './modules/account';
+import { Module } from './modules/';
 import { projectModule } from './modules/project';
 import { viewerModule } from './modules/viewer';
 export * from './auth';
@@ -14,6 +15,7 @@ import { permissionMiddleware } from './middlewares/permission';
 const executableSchema = makeExecutableSchema<GraphqlServerContext>({
   typeDefs,
   resolvers: merge.all<Resolvers<GraphqlServerContext>>([
+    Module.resolvers,
     projectModule.resolvers,
     accountModule.resolvers,
     viewerModule.resolvers,
