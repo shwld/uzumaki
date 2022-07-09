1b2c740951feb07365c1d28fccaac04bc60746a8
shwld <shu.account@outlook.com>
2022-07-10 06:46:50 +0900

    ✨feat: create test story

--- infrastructures/db/src/testData/storyFactory.ts
+++ infrastructures/db/src/testData/storyFactory.ts
@@ -1,4 +1,9 @@
-import { buildStory, StoryEntity, StoryEntityFields } from 'core-domain';
+import {
+  buildStory,
+  ProjectEntity,
+  StoryEntity,
+  StoryEntityFields,
+} from 'core-domain';
 import { faker } from '@faker-js/faker';
 import { storyRepository } from '../repositories/storyRepository';
 
@@ -6,7 +11,16 @@ export const buildTestStoryAttributes = (
   fields?: Partial<StoryEntityFields>
 ): StoryEntityFields => {
   return {
-    id: faker.random.alpha(10),
+    id: faker.datatype.uuid(),
+    title: faker.lorem.slug(),
+    description: faker.lorem.text(),
+    state: 'UNSTARTED',
+    kind: 'CHORE',
+    points: faker.datatype.number(),
+    releaseDate: faker.datatype.datetime(),
+    position: 'DONE',
+    priority: faker.datatype.number(),
+    projectId: faker.datatype.uuid(),
     createdAt: faker.date.past(),
     updatedAt: faker.date.past(),
     ...fields,
@@ -14,18 +28,22 @@ export const buildTestStoryAttributes = (
 };
 
 export const buildTestStory = (
+  project: ProjectEntity,
   fields?: Partial<StoryEntityFields>
 ): StoryEntity => {
   return buildStory({
     ...buildTestStoryAttributes(fields),
+    project,
   });
 };
 
 export const createTestStory = (
+  project: ProjectEntity,
   fields?: Partial<StoryEntityFields>
 ): Promise<StoryEntity> => {
   const story = buildStory({
     ...buildTestStoryAttributes(fields),
+    project,
   });
 
   return storyRepository.create(story);
