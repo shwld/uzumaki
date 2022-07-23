9d96e420fe571381edbb90132a6d6dd9197ca783
shwld <shu.account@outlook.com>
2022-07-24 07:48:10 +0900

    🐞fix: lint

--- packages/eslint-config-acme/index.js
+++ packages/eslint-config-acme/index.js
@@ -1,6 +1,14 @@
 module.exports = {
   extends: ['next', 'prettier'],
-  ignorePatterns: ['*.hbs', '*.json', '*.sql', '*.prisma', '*.toml', '*.yml'],
+  ignorePatterns: [
+    '*.hbs',
+    '*.json',
+    '*.sql',
+    '*.graphql',
+    '*.prisma',
+    '*.toml',
+    '*.yml',
+  ],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
     'react/jsx-key': 'off',
