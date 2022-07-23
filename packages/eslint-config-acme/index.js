319b34f3e29f82737bc9002af4013f4c5fcc7567
shwld <shu.account@outlook.com>
2022-07-24 07:29:58 +0900

    👷chore: setup eslint

--- packages/eslint-config-acme/index.js
+++ packages/eslint-config-acme/index.js
@@ -1,7 +1,8 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  extends: ['next', 'prettier'],
+  ignorePatterns: ['*.hbs', '*.json', '*.sql', '*.prisma', '*.toml', '*.yml'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-key': 'off',
   },
 };
