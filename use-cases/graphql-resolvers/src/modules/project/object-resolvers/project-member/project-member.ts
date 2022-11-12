7c20c233e0a362511e55e505aa3f7adc7140a2ad
shwld <shu.account@outlook.com>
2022-11-13 07:38:44 +0900

    🐞fix: Cannot return null for non-nullable field ProjectMember.name.

--- use-cases/graphql-resolvers/src/modules/project/object-resolvers/project-member/project-member.ts
+++ use-cases/graphql-resolvers/src/modules/project/object-resolvers/project-member/project-member.ts
@@ -4,4 +4,10 @@ export const ProjectMember: ProjectMemberResolvers = {
   isMe(parent, _args, context, _info) {
     return context.currentUser?.id === parent.userId;
   },
+  name(parent, _args, context, _info) {
+    return parent.user.name;
+  },
+  avatarImageUrl(parent, _args, context, _info) {
+    return parent.user.avatarImageUrl;
+  },
 };
